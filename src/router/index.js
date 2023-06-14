41e6fd77ecb8d8f598832c1e6b23c6354bb2edd5
beller <brades8976@outlook.com>
2023-06-13 19:12:13 -0500

    feat: create saveview for game

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,18 @@ import HomeView from "../views/HomeView.vue";
 import ListView from "../views/employees/ListView.vue";
 import Employee from "../views/employees/Employee.vue";
 import SaveView from "../views/employees/SaveView.vue";
+import GameListView from "../views/games/GameListView.vue";
+import Game from "../views/games/Game.vue";
+import GameSaveView from "../views/games/GameSaveView.vue";
 
 const routes = [
     { path: '/', component: HomeView },
     { path: '/empleados', component: ListView },
     { path: '/empleados/:id', component: Employee },
     { path: '/empleados/nuevo', component: SaveView },
+    { path: '/juegos', component:GameListVie},
+    { path: '/juegos/:id', component: Game},
+    { path: '/juegos/nuevo', component: GameSaveView}
 ];
 
 const router = createRouter({
---
+++ src/views/games/GameSaveView.vue
@@ -0,0 +1,53 @@
+<template>
+    <form @submit.prevent="saveEmployee">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Nombre de la atracción</label>
+            <input type="text" v-model="game.name" class="form-control" id="exampleInputPassword1">
+            
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Capacidad de personas</label>
+            <input type="number" v-model="game.capacityPersons" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">duración</label>
+            <input type="number" v-model="game.duration" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Precio de venta</label>
+            <input type="number" v-model="game.salesPrice" class="form-control" id="exampleInputPassword1">
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Guardar</button>
+    </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            game: {
+                name: '',
+                capacityPersons: 0,
+                duration:0,
+                salesPrice: 0,
+                registrationDate:''
+            }
+        }
+    },
+    methods: {
+        saveGame() {
+            this.game.registrationDate = new Date().toISOString();
+            axios.post('/games', this.game).then(
+                response => {
+                    console.log(response.status);
+                    location.href = "/juegos";
+                }
+            );
+        }
+    }
+}
+</script>
